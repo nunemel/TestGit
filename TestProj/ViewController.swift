606f54606819edc59aecd8175b7541f47364ca89
nunemel <nune.melikyan@yahoo.com>
2022-09-22 22:33:00 +0400

    Initial Commit

---
+++ TestProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProj
+//
+//  Created by Nune Melikyan on 22.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
